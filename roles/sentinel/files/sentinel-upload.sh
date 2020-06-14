e988a27e7f3df7be57285dbab1b1874dad3f7e19
Timothy Perrett <timothy@getintheloop.eu>
2020-06-13 21:24:16 -0700

    fix the uploading via sftp

--- roles/sentinel/files/sentinel-upload.sh
+++ roles/sentinel/files/sentinel-upload.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-set -x
+set -e
 
-lftp -u "${SFTP_USERNAME},${SFTP_PASSWORD}" sftp://${SFTP_HOST} <<EOF
-cd sentinel
-put /tmp/sentinel.jpg -o /sentinel/$(date +%Y-%m-%d-%H.%M.%S).jpg
+REMOTE_BASEDIR="/sentinel"
+REMOTE_DIR="${REMOTE_BASEDIR}/$(date +%Y-%m-%d)"
+REMOTE_PATH="${REMOTE_DIR}/$(date +%Y-%m-%d-%H.%M.%S).jpg"
+
+echo "--> uploading file ${REMOTE_PATH}"
+
+lftp -u "${SFTP_USERNAME},${SFTP_PASSWORD}" "sftp://${SFTP_HOST}" <<EOF
+set sftp:auto-confirm yes
+set net:timeout 5
+set net:max-retries 2
+set net:reconnect-interval-base 5
+mkdir -p ${REMOTE_DIR}
+cd ${REMOTE_DIR}
+put /tmp/sentinel.jpg -o ${REMOTE_PATH}
 bye
 EOF
