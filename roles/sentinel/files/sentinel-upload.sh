6524d6b92e616c7476033872b9b553c7f47ef0a5
Timothy Perrett <timothy@getintheloop.eu>
2020-06-13 22:21:52 -0700

    adapt the upload, add throttling

--- roles/sentinel/files/sentinel-upload.sh
+++ roles/sentinel/files/sentinel-upload.sh
@@ -5,10 +5,20 @@ set -e
 REMOTE_BASEDIR="/sentinel"
 REMOTE_DIR="${REMOTE_BASEDIR}/$(date +%Y-%m-%d)"
 REMOTE_PATH="${REMOTE_DIR}/$(date +%Y-%m-%d-%H.%M.%S).jpg"
+UPLOAD_CADANCE="5" # in minutes
 
-echo "--> uploading file ${REMOTE_PATH}"
+# handy function to do math with real nums
+calc(){ 
+    awk "BEGIN { print "$*" }"
+}
 
-lftp -u "${SFTP_USERNAME},${SFTP_PASSWORD}" "sftp://${SFTP_HOST}" <<EOF
+# in order to throttle the amount of uploads we have (and 
+# therefore consumption of bandwidth), artificially eliminate
+# actions that happen on minutes that are not in ${UPLOAD_CADANCE}
+# minutely divisions.
+if [[ $(calc $(date +%M)%${UPLOAD_CADANCE}) == 0 ]]; then 
+    echo "--> uploading file ${REMOTE_PATH}"
+    lftp -u "${SFTP_USERNAME},${SFTP_PASSWORD}" "sftp://${SFTP_HOST}" <<EOF
 set sftp:auto-confirm yes
 set net:timeout 5
 set net:max-retries 2
@@ -18,3 +28,7 @@ cd ${REMOTE_DIR}
 put /tmp/sentinel.jpg -o ${REMOTE_PATH}
 bye
 EOF
+
+else 
+    echo "--> skipping upload to conserve bandwidth"; 
+fi
