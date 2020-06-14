5e9b46422a527659d0a9b9be6365e1d042b45663
Timothy Perrett <timothy@getintheloop.eu>
2020-06-13 22:19:28 -0700

    Reduce the frame rate initially

--- roles/sentinel/files/sentinel.sh
+++ roles/sentinel/files/sentinel.sh
@@ -7,7 +7,7 @@ OUTPUT_TITLE="eventually.house"
 OUTPUT_RESOLUTION="1920x1080"
 
 fswebcam -r "${OUTPUT_RESOLUTION}" \
-  --jpeg 85 -F 50 \
+  --jpeg 85 -F 30 \
   --banner-colour \#000000 \
   --line-colour \#ffffff \
   --title "${OUTPUT_TITLE} ($(date +%B))" \
